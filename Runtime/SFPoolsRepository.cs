5282a77b110e96a56efcd25dd6e55d2f2f8ec27a
i.khurani <huran438@gmail.com>
2023-02-18 01:36:23 +0300

    Added Generator

--- Runtime/SFPoolsRepository.cs
+++ Runtime/SFPoolsRepository.cs
@@ -6,9 +6,38 @@ using UnityEngine;
 
 namespace SFramework.Pools.Runtime
 {
-    public class SFPoolsRepository : SFRepository
+    public class SFPoolsRepository : SFRepository, ISFRepositoryGenerator
     {
         public override ISFNode[] Nodes => Groups;
         public SFPrefabsGroupContainer[] Groups = Array.Empty<SFPrefabsGroupContainer>();
+
+        public void GetGenerationData(out SFGenerationData[] generationData)
+        {
+            var pools = new HashSet<string>();
+            var prefabs = new HashSet<string>();
+
+            foreach (var layer0 in Groups)
+            {
+                pools.Add($"{_Name}/{layer0._Name}");
+                foreach (var layer1 in layer0.Prefabs)
+                {
+                    prefabs.Add($"{_Name}/{layer0._Name}/{layer1._Name}");
+                }
+            }
+
+            generationData = new[]
+            {
+                new SFGenerationData
+                {
+                    FileName = "SFPrefabs",
+                    Properties = prefabs
+                },
+                new SFGenerationData
+                {
+                    FileName = "SFPools",
+                    Properties = pools
+                }
+            };
+        }
     }
 }
