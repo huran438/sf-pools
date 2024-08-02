26ecc10ed27cf4db8c1dc44c9837cb4372d1bd21
i.khurani <huran438@gmail.com>
2024-08-03 00:06:12 +0300

    New SFConfigs logic

--- Runtime/SFPoolsConfig.cs
+++ Runtime/SFPoolsConfig.cs
@@ -6,38 +6,9 @@ using UnityEngine;
 
 namespace SFramework.Pools.Runtime
 {
-    public class SFPoolsConfig : SFConfig, ISFConfigsGenerator
+    public class SFPoolsConfig : SFNodesConfig
     {
         public override ISFConfigNode[] Children => Groups;
         public SFPrefabsGroupNode[] Groups = Array.Empty<SFPrefabsGroupNode>();
-
-        public void GetGenerationData(out SFGenerationData[] generationData)
-        {
-            var pools = new HashSet<string>();
-            var prefabs = new HashSet<string>();
-
-            foreach (var layer0 in Groups)
-            {
-                pools.Add($"{Id}/{layer0.Id}");
-                foreach (var layer1 in layer0.Prefabs)
-                {
-                    prefabs.Add($"{Id}/{layer0.Id}/{layer1.Id}");
-                }
-            }
-
-            generationData = new[]
-            {
-                new SFGenerationData
-                {
-                    FileName = "SFPrefabs",
-                    Properties = prefabs
-                },
-                new SFGenerationData
-                {
-                    FileName = "SFPools",
-                    Properties = pools
-                }
-            };
-        }
     }
 }
